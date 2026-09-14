367f31ccc13a0496dbf70c3ac0952a140ab9619b
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-09-13 21:53:08 -0300

    fix: use Ktor EngineMain startup

--- src/main/kotlin/com/superplanner/gateway/Application.kt
+++ src/main/kotlin/com/superplanner/gateway/Application.kt
@@ -1,25 +1,14 @@
 package com.superplanner.gateway
 
-import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-
-fun main() {
-    embeddedServer(
-        Netty,
-        port = System.getenv("PORT")?.toIntOrNull() ?: 8080,
-        host = "0.0.0.0",
-        module = Application::module
-    ).start(wait = true)
-}
 
 fun Application.module() {
     val geminiService = GeminiService()
