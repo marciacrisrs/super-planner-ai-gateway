d5e26388e841697b79b20341be1d6e045510dab0
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-09-15 01:47:43 -0300

    style: keep gateway imports ktlint-compatible

--- src/main/kotlin/com/superplanner/gateway/Application.kt
+++ src/main/kotlin/com/superplanner/gateway/Application.kt
@@ -4,8 +4,8 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
-import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
